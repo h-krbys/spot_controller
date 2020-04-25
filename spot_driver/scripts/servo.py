ebff036579564824ac4b071ba2cc1d050be4ca7e
spot <you@example.com>
2020-04-26 00:14:11 +0900

    experiment: research joint motions by joystick

--- spot_driver/scripts/servo.py
+++ spot_driver/scripts/servo.py
@@ -8,7 +8,7 @@ class Servo:
 
     def __init__(self):
         # Subscribe 12 joint angles [rad]
-        self.sub = rospy.Subscriber("joint", Float32MultiArray, self.jointCallback)
+        self.sub = rospy.Subscriber("joint", Float32MultiArray, self.jointCallback, queue_size=10)
 
         # Initialise the PCA9685 using desired address & bus
         self.driver = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
@@ -18,8 +18,14 @@ class Servo:
         self.limit = rospy.get_param('limit')
 
     def jointCallback(self, msg):
+        print(msg.data[0])
+        #self.driver.set_pwm(0, 0, int(self.limit[0]['zero']) + int(450 * msg.data[0] / 3.14159))
+        #self.driver.set_pwm(1, 0, int(self.limit[1]['zero']) + int(450 * msg.data[1] / 3.14159))
+        #self.driver.set_pwm(2, 0, int(self.limit[2]['zero']) + int(450 * msg.data[2] / 3.14159))
+        #self.driver.set_pwm(3, 0, int(self.limit[3]['zero']) + int(450 * msg.data[3] / 3.14159))
         for i in range(12):
-            self.driver.set_pwm(i, 0, limit[i]['zero'] + 450 * msg.data[i] / 3.14159)
+            self.driver.set_pwm(i, 0, int(self.limit[i]['zero']) + int(450 * msg.data[i] / 3.14159))
+            #self.driver.set_pwm(i, 0, int(self.limit[i]['zero']) + 450) )
 
 if __name__ == '__main__':
     rospy.init_node('servo')
